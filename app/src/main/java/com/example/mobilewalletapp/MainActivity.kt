581e84dfc62e27c4c77f99ae33104155ed8f8ef6
Sharonoito <oitosharonakoth@gmail.com>
2021-08-26 19:56:22 +0300

    first commit

--- app/src/main/java/com/example/mobilewalletapp/MainActivity.kt
+++ app/src/main/java/com/example/mobilewalletapp/MainActivity.kt
@@ -5,38 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tvaccountName: TextView
+    lateinit var tvCurrency: TextView
+    lateinit var tvBalance: TextView
+    lateinit var tvtransactionAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        castViews()
 
+
+    }
+    fun castViews(){
+        tvaccountName=findViewById(R.id.tvaccountName)
+        tvCurrency=findViewById(R.id.tvCurrency)
+        tvBalance=findViewById(R.id.tvBalance)
+        tvtransactionAmount=findViewById(R.id.tvtransactionAmount)
     }
 }
 
-//class MainActivity : AppCompatActivity() {
-//    lateinit var etEmail: EditText
-//    lateinit var etPassword: EditText
-//    lateinit var btnLogin: Button
-//    lateinit var btnSignup: Button
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        castViews()
-//        login()
-//        signup()
-//
-//    }
-//    fun castViews(){
-//        etEmail=findViewById(R.id.etName)
-//        etPassword=findViewById(R.id.etPassword)
-//        btnLogin=findViewById(R.id.btnLogin)
-//        btnSignup=findViewById(R.id.btnSignup)
-//    }
-//    fun login(){
-//        btnLogin.setOnClickListener {
-//            val intent= Intent(baseContext,MainActivity::class.java)
-//            startActivity(intent)
-//        }
-//    }
+
